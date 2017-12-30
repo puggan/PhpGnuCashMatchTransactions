1b235499ee43ab59bdb6a7a8cac9557ab0ed5ca7
Thorbjörn Backlund <thorbjorn@spiro.se>
2017-12-30 20:29:15 +0100

    cron-job updated

--- cron.php
+++ cron.php
@@ -1,30 +1,21 @@
 <?php
 
-	require_once(__DIR__ . "/auth.php");
-
-	$db = Auth::new_db();
+	require_once(__DIR__ . "/bank_funk.php");
+	$bi = new Bank_interface();
+	$db = $bi->db();
 
 	$db->write("DELETE gnclock FROM gnclock INNER JOIN gnclock_ts USING (PID, Hostname) WHERE ts < NOW() - INTERVAL 1 HOUR");
 	$db->write("UPDATE transactions SET post_date = post_date + INTERVAL 12 HOUR WHERE DATE(post_date) < DATE(CONVERT_TZ(post_date, 'UTC', 'SYSTEM'))");
+	$db->write("UPDATE bank_transactions LEFT JOIN splits ON (splits.guid = bank_transactions.bank_tid) SET bank_transactions.bank_tid = NULL WHERE splits.guid IS NULL AND bank_transactions.bank_tid IS NOT NULL");
 
-/*
+	$query = "SELECT * FROM bank_transactions WHERE bank_tid IS NULL AND bdate >= '2016-09-01'";
+	/**
+	 * @var int $row_nr
+	 * @var table_bank_transactions $bank_row
+	 */
+	foreach($db->g_objects($query, 'bank_t_row', table_bank_transactions::class) as $row_nr => $bank_row) {
+		$bank_row->save_cache($db);
+	}
 
-CREATE TABLE `gnclock_ts` (
-  `Hostname` varchar(255) NOT NULL DEFAULT '',
-  `PID` int(11) NOT NULL DEFAULT '0',
-  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`PID`,`Hostname`),
-  CONSTRAINT `gnclock_ts_ibfk_1` FOREIGN KEY (`PID`, `Hostname`) REFERENCES `gnclock` (`PID`, `Hostname`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
-DELIMITER #
-
-CREATE TRIGGER gnclock_add AFTER INSERT ON gnclock
-FOR EACH ROW
-BEGIN
-	INSERT INTO gnclock_ts(Hostname, PID) VALUES (new.Hostname, new.PID);
-END#
-
-DELIMITER ;
-
-*/
+	$query = "DELETE bank_transactions_cache FROM bank_transactions_cache LEFT JOIN bank_transactions USING (bank_t_row) WHERE bank_transactions.bank_t_row IS NULL OR bank_tid IS NOT NULL OR bdate < '2016-09-01'";
+	$db->write($query);
